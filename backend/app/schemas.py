c7eeac85f19556573375daa9d83357ed326f9a07
Shivansh <shivanshyadav12@gmail.com>
2026-05-11 17:07:57 +0530

    Created schemas for req and res from the problems and application api

---
+++ backend/app/schemas.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel
+from datetime import time
+
+class ProblemsBase(BaseModel):
+
+    title: str
+    problem_url: str
+    difficulty: str 
+    category: str
+    status: str 
+    confidence_level: str
+    time_taken: time
+    solution_type: str 
+
+class ProblemsRequest(ProblemsBase):
+    pass
+
+class ProblemsResponse(ProblemsBase):
+    id: int
+    class Config:
+        from_attributes = True
+
+class ApplicationsBase(BaseModel):
+
+    company : str
+    role : str
+    job_desc_url : str
+    job_type : str
+    location : str
+    status : str
+    source : str
+    interview_date : time
+    interview_type : str
+    rounds_completed : str
+    recruiter_mail : str
+    min_salary : int 
+    max_salary : int 
+
+class ApplicationsRequest(ApplicationsBase):
+    pass
+
+class ApplicationResponse(ApplicationsBase):
+    id: int
+    class Config:
+        from_attributes = True
